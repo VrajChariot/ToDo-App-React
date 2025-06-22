95482f9adbc913563a7a87a5fa697106b6d26085
Vraj Shah <vraj0410shah@gmail.com>
2025-06-22 18:52:33 +0200

    added local storage and made site responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,46 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [showFinished, setshowFinished] = useState(true);
 
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, [])
+  
+
+
+  const localStorageTodo = (updatedTodos) => { 
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  }
   const handleInputChange = (e) => {
     setinputText(e.target.value);
-    console.log(e.target.value);
   }
   const handleAddTodo = () => {
-    console.log(`inputText is: ${inputText}`);
     if (inputText.length > 3) {
       let newTodos = [...todos, {inputText, id:uuidv4(), isCompleted: false}];
       setTodos(newTodos);
-      console.log(`New todo added: ${inputText}`);
-      console.log(newTodos);
+      localStorageTodo(newTodos);
+      setinputText("");
     }
-    setinputText("");
+
   }
   const handleDeleteTodo = (id) => {
-    console.log(`Todo with id ${id} deleted`);
     let newTodos = todos.filter(item => item.id !== id);
     setTodos(newTodos);
+    localStorageTodo(newTodos);
   }
   const handleEditTodo = (id) => {
-    console.log(`Todo with id ${id} is edited`);
     setinputText(todos.find(item => item.id === id).inputText);
     let newTodos = todos.filter(item => item.id !== id);
     setTodos(newTodos);
   }
   const handleCheckbox = (id) => {
-    console.log(`Todo with id ${id} id checked`);
     let newTodos = [...todos];
     let todoToUpdate = newTodos.find(item => item.id === id);
     todoToUpdate.isCompleted = !todoToUpdate.isCompleted;
     setTodos(newTodos);
+    localStorageTodo(newTodos);
   }
 
   const handleShowfinished = () => { 
@@ -46,8 +55,8 @@ function App() {
    }
 
   return (
-    <main className='flex items-center justify-center h-screen'>
-      <div className='w-1/3 h-[80vh] rounded-3xl bg-violet-300 border border-violet-800 shadow-xl p-4 '>
+    <main className='flex items-center justify-center h-screen p-4'>
+      <div className='w-full h-full rounded-3xl bg-violet-300 border border-violet-800 shadow-xl p-4 sm:w-[80%] sm:h-[80vh] xl:w-1/3'>
         <h2 className='text-2xl font-bold text-center'>Your own Task Manager - Taskify</h2>
         <div className='flex gap-2 w-full my-4'>
           <input type="text" className='bg-white rounded-3xl w-full p-2' onChange={handleInputChange} value={inputText}/>
